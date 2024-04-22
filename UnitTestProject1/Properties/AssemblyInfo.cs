c49ec6d312fa45166eab9a829276c32a8c8e4ffe
Alka <alka.kumari@gmail.com>
2024-04-22 18:31:54 +0200

    Add Initial Files

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0908be55-9d65-4fcb-9eba-7e5c7ea859cc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver;
+           
+            ChromeOptions options = new ChromeOptions();
+            // Path to the CEF executable.
+            options.BinaryLocation=@"C:\Program Files\Stark\Sales Application\STARKSalesApplication.exe";
+            // Port to communicate on. Required starting with ChromeDriver v2.41.
+            options.AddArgument("remote-debugging-port=12345");
+            //options.AddArgument("");
+
+            driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("https://beta.sales.stark.dk.aka.stark.group/");
+            Thread.Sleep(5000);
+            driver.FindElement(By.Name("loginfmt")).SendKeys("praveen.kumar@stark.dk");
+
+        }
+    }
+}
